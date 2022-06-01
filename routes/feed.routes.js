6b0f988f07cefbf2451d23b6e823adb850bff4ae
Juan Jose Ferran Carmona <jferran@gmail.com>
2022-06-01 19:56:35 +0200

    API /api/feed

---
+++ routes/feed.routes.js
@@ -0,0 +1,47 @@
+const Feed = require("../models/Feed.model");
+
+const router = require("express").Router();
+//const isAuthenticated = require("../middlewares/isAuthenticated")
+
+router.get("/", async (req, res, next) =>{
+    try {
+        const response = await Feed.find()
+        res.json(response)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.post("/", async (req, res, next) => {
+    const { name, sourceUrl } = req.body
+    try {
+        const response = await Feed.create({name, sourceUrl})
+        res.json(response)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.get("/:id", async (req, res, next) => {
+    const { id } = req.params
+    try {
+        const response = await Feed.findById(id)
+        res.json(response)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.delete("/:id", async (req, res, next) => {
+    const {id} = req.params
+    try {
+        await Feed.findByIdAndDelete(id)
+        res.json("Feed was deleted") 
+    } catch (error) {
+        next(error)        
+    }
+})
+
+
+
+module.exports = router;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -7,4 +7,10 @@ router.get("/", (req, res, next) => {
 const authRoutes = require('./auth.routes')
 router.use('/auth', authRoutes)
 
+const feedRoutes = require('./feed.routes')
+router.use('/feed', feedRoutes)
+
+const newsRoutes = require('./news.routes')
+router.use('/news', newsRoutes)
+
 module.exports = router;
