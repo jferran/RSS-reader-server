2cd2fcade5ca7211ca9b780fb6d56f0632043c25
Juan Jose Ferran Carmona <jferran@gmail.com>
2022-06-04 16:11:33 +0200

    adding news to user after subscribing

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const UserModel = require("../models/User.model")
+const FeedModel = require("../models/Feed.model")
 //const isAuthenticated = require("../middlewares/isAuthenticated")
 
 // gets all subscribed Feeds
@@ -23,7 +24,7 @@ router.get("/:userID/feed/:id", async (req, res, next) => {
     try {
         //https://www.spektor.dev/filtering-values-in-nested-arrays-mongodb/
         const response = await UserModel.findById(userID, {subscribedFeeds: {$elemMatch: {feed: id}}, projection:{subscribedFeeds: 1}}).populate('subscribedFeeds.feed').lean()
-        const test = await UserModel.findByIdAndUpdate(userID, {$addToSet: {newsList: {entry: id}}})
+        //const test = await UserModel.findByIdAndUpdate(userID, {$addToSet: {newsList: {entry: id}}})
         //const response = await UserModel.findById(userID, 'subscribedFeeds.feed').populate('subscribedFeeds.feed')
         //const response = await UserModel.findById(userID).select('subscribedFeeds')
         //const response = await UserModel.findById(userID, {projection: {subscribedFeeds: 1}})
@@ -47,7 +48,12 @@ router.get("/:userID/feed/:id/subscribe", async (req, res, next) => {
         
         //we have to decide, if we want _id or feed for subscribed feed, probably just _id
         //const response = await UserModel.findByIdAndUpdate(userID, {$addToSet: {subscribedFeeds: {_id: id, feed: id}}}).populate('subscribedFeeds._id')
-        const response = await UserModel.findByIdAndUpdate(userID, {$addToSet: {subscribedFeeds: {_id: id}}}).populate('subscribedFeeds._id')
+        const news = await FeedModel.findById(id).select('news')
+        //console.log(news.news)
+        //newsArrayOfObjects = news.news.reduce((acc, item)=>{ [...acc, {'_id': item}]}, [])
+        newsArrayOfObjects = news.news.map((element)=>{return {'_id': element}})
+        //console.log("arrayofobj:", newsArrayOfObjects)
+        const response = await UserModel.findByIdAndUpdate(userID, {$addToSet: {subscribedFeeds: {_id: id}}, $addToSet: {newsList: newsArrayOfObjects}}).populate('subscribedFeeds._id')
         //const test = await UserModel.findByIdAndUpdate(userID, {$addToSet: {newsList: {}}})
         //.populate('subscribedFeeds.feed', 'name')
 
